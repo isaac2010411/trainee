d6ddad05f98ccec0fa619437f8b9fb347dfb5396
Isaac <isaac2010411@gmail.com>
2021-01-19 11:17:24 -0300

    fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ server.use(express.static(publicPath));
 
  server.use('*', cors(
     { 
-        origin: `https://traineetest.herokuapp.com:${PORT}` ,
+        origin: `http://localhost:${PORT}` ,
         credentials:true
     }
      )
@@ -54,7 +54,7 @@ server.use('/graphql',
 
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
-  subscriptionsEndpoint: `ws://https://traineetest.herokuapp.com:${PORT}/subscriptions`,
+  subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`,
 }));
 server.get('*',cors(), (req, res) => {    
     console.log(req.headers)
